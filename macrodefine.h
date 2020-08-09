5e59f576f5d3dbbcdbe894f6257d8e66b7bc2bcd
antogit-sys <antonioperrgiorgi@gmail.com>
2020-08-09 22:50:43 +0200

    lol

--- macrodefine.h
+++ macrodefine.h
@@ -6,7 +6,7 @@
 #	define USEXAMPLE	"\t./passwdGenerator instagram 32 --opts 1\n"
 #elif _WIN32 || _WIN64 || __CYGWIN__ || __CYGWIN32__ //altrimenti se è un sistema winzzoz dichiara
 #	define USE	"\tnamefile.exe [name] [nchar] --opts [n]\n"
-#	define USEXAMPLE	"\t.passwdGenerator.exe instagram 32 --opts 1\n"
+#	define USEXAMPLE	"\tpasswdGenerator.exe instagram 32 --opts 1\n"
 #endif
 #define controlAlloc(x) if(x==NULL){printf("\nallocazione fallita...");return 0;}
 #define C 100
