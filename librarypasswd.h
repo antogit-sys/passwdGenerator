059a5ee46e5eaadfb9aa05ba8669aeaf98abb581
Antonio <64706936+antogit-sys@users.noreply.github.com>
2021-01-06 11:06:58 +0100

    Delete librarypasswd.h

--- librarypasswd.h
+++
@@ -1,5 +0,0 @@
-/* PROTOTIPI DI FUNZIONI */
-void intro(); //help command
-char *alloc(int); //allocazione array char
-int randNum(char[],int,int); //casual generated
-void fileW(char[],char[],int); //memorizza sul file: nome e password
